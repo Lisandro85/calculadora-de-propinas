aa2d50fb952ac56d8871fc7fbab5df5ae734958c
Lisandro85 <lisandrobedotti@hotmail.com>
2025-01-13 11:57:18 +0100

    totales/consumo

--- calculadorapropinas/src/App.tsx
+++ calculadorapropinas/src/App.tsx
@@ -1,6 +1,11 @@
+import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
 import { menuItems } from "./data/db";
+import useOrder from "./hooks/userOrder";
+import OrderTotal from "./components/OrderTotal";
 
 function App() {
+  const { addItem, order, removeItem } = useOrder();
   return (
     <>
       <header className=" bg-teal-500 py-5">
@@ -9,11 +14,17 @@ function App() {
         </h1>
       </header>
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menu</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menu</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
         </div>
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 raunded-lg space-y-10">
+          <OrderContents order={order} removeItem={removeItem} />
+          <OrderTotal />
         </div>
       </main>
     </>
